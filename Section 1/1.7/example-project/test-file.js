72d747285d7e890176fc0eb59585d771a9082224
gokartgo <gokart.007@hotmail.com>
2020-11-28 19:55:49 +0700

    finish 1.7

--- Section 1/1.7/example-project/test-file.js
+++ Section 1/1.7/example-project/test-file.js
@@ -1,27 +1,29 @@
 let todos = [
     {
-        id: 1,
-        task: "Commit code",
+        id: 1,     
+        task: 'Commit code',
         completed: true
     },
     {
         id: 2, 
-        task: "Push code",
+        task: 'Push code',
         completed: false
     },
     {
         id: 3,
-        task: "Leave building",
+        task: 'Leave building',
         completed: false
     }
-];
+]
 
 // console.log("All todos: ");
 // console.log(todos);
 
-let completed = [];
+let completed = []
 for(let todo of todos) {
     if(todo.completed) {
-        completed.push(todo);
+        completed.push(todo)
+        completed.push(todo)
+        completed.push(todo)
     }
 }
